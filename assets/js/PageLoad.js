40ced77d6019ec1c92a8df856643b43acff4e1aa
MaxPhilips <mphilips@iastate.edu>
2013-10-16 22:34:02 -0500

    Finished AJAX content injection

--- assets/js/PageLoad.js
+++ assets/js/PageLoad.js
@@ -1,33 +1,51 @@
 $(document).ready(function() {
 	$('#new').on('click', function(event) {
 		$.ajax({
-			url: '../../new.html',
+			url: 'new.html',
 			type: 'POST',
 			success: function() {}
+		}).done(function(html) {
+			$("#content").html(html);
 		});
 		event.preventDefault;
 	});
 	$('#current').on('click', function(event) {
 		$.ajax({
-			url: '../../current.html',
+			url: 'current.html',
 			type: 'POST',
 			success: function() {}
+		}).done(function(html) {
+			$("#content").html(html);
 		});
 		event.preventDefault;
 	});
 	$('#highScores').on('click', function(event) {
 		$.ajax({
-			url: '../../highscores.html',
+			url: 'highscores.html',
 			type: 'POST',
 			success: function() {}
+		}).done(function(html) {
+			$("#content").html(html);
 		});
 		event.preventDefault;
 	});
 	$('#help').on('click', function(event) {
 		$.ajax({
-			url: '../../help.html',
+			url: 'help.html',
 			type: 'POST',
 			success: function() {}
+		}).done(function(html) {
+			$("#content").html(html);
+		});
+		event.preventDefault;
+	});
+	$('#settings').on('click', function(event) {
+		$.ajax({
+			url: 'accountsettings.html',
+			type: 'POST',
+			success: function() {}
+		}).done(function(html) {
+			$("#content").html(html);
 		});
 		event.preventDefault;
 	});
