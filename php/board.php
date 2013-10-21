73af320c147dea65c6b49c0231e1efdce3f83c54
Holden Rehg <holdenrehg@gmail.com>
2013-10-21 18:40:22 -0500

    Update board.php

--- php/board.php
+++ php/board.php
@@ -1,8 +1,31 @@
 <?php
 
+  require('boardspace.php');
+
   class Board 
   {
-
+  
+    private $state = array(array());
+  
+    public function __construct() {
+      $this->makeBlankBoard();
+    }
+    
+    /**
+     * Return a JSON representation of this board 
+     **/
+    public function getJSONBoard() {
+      
+      // TODO
+    }
+    
+    /**
+     * Initialize the board with BoardSpace's 
+     **/
+    private function makeBlankBoard() {
+      
+      // TODO
+    }
   }
 
 ?>
