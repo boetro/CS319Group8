7717f698449c08ba1adb0c0b7009ab5d6abed4de
Holden Rehg <holdenrehg@gmail.com>
2013-10-29 16:50:21 -0500

    set up player class to represent database table, setup getters, setters, and construct

--- php/classes/player.php
+++ php/classes/player.php
@@ -2,15 +2,46 @@
 
 	class Player
 	{
-		private $gamerid;
-		private $passhash;
-		private $email;
 		
-		public function __construct($gamerid, $passhash, $email)
+		/**
+		 * @column idplayer
+		 * @auto
+		 */
+		private $id;
+
+		/**
+		 * @column created_at
+		 * @auto
+		 **/
+		private $created_at;
+		
+		/**
+		 * @column email
+		 */
+		private $email;
+
+		/**
+		 * @column pass_hash
+		 */
+		private $pass_hash;
+		
+		/**
+		 * @column gamertag
+		 **/
+		private $gamertag;
+
+		/**
+		 * @column Theme_Color
+		 **/
+		// TODO change caseing in database
+		private $theme_color;
+		
+		public function __construct($email, $pass_hash, $gamertag, $theme_color)
 		{
-			$this->gamerid = $gamerid;
-			$this->passhash = $passhash;
 			$this->email = $email;
+			$this->pass_hash = $pass_hash;
+			$this->gamertag = $gamertag;
+			$this->theme_color = $theme_color;
 		}
 
 		/** 
@@ -19,5 +50,37 @@
 		 */
 		public function push() {
 
+		}
+
+		/**
+		 * Magic getter
+		 *
+		 * @param property to get
+		 * @return property or false if it does not exist
+		 */
+		public function __get($property) {
+			
+			if (property_exists($this, $property)) {
+		      return $this->$property;
+		    }
+
+		    return false;
+		}
+
+		/**
+		 * Magic setter
+		 *
+		 * @param property to set
+		 * @param value to set the property to
+		 * @return true on successfull set, false on fail
+		 */
+		public function __set($property, $value) {
+			
+			if (property_exists($this, $property)) {
+		      $this->$property = $value;
+		      return true;
+		    }
+
+		    return false;
 		}
 	}
