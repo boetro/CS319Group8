10c22174f408cf77c4f89c1f12b2c9931d5952d1
holdenrehg <holdenrehg@gmail.com>
2013-11-21 17:43:04 -0600

    skeleton ajax class

---
+++ php/classes/ajax.php
@@ -0,0 +1,15 @@
+<?php
+
+	/**
+	 * TODO if we have time to reduce number of scripts written
+	 * Write a single ajax handle script that routes POST/GET requests to desired function 
+	 */
+	class Ajax
+	{
+		public function __construct()
+		{
+
+		}
+	}
+
+?>
